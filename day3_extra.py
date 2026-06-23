53d6eea8d8132d22b6467304ef8fbb945987f608
Mario Ibrahimi <295660683+marioibrahimi@users.noreply.github.com>
2026-06-23 23:05:58 +0300

    Day 3: extra variables and f-string practice

---
+++ day3_extra.py
@@ -0,0 +1,12 @@
+# Day 3 extra practice
+
+city="Bucharest"
+current_income = 800
+target_income = 1500
+skill = "Python automation"
+
+print("Day 3 Extra Practice")
+print(f"I currently lice in {city}.")
+print(f"My current net income is about ${current_income} net per month.")
+print(f"My first target is ${target_income} net per month.")
+print(f"The skills I am building is {skill}.")
