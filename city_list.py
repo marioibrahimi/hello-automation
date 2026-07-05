a2e0baf66950e8f727ab761f920149797c6d7e99
Mario Ibrahimi <295660683+marioibrahimi@users.noreply.github.com>
2026-07-05 17:06:19 +0300

    Day 15: practice for loops with city list

---
+++ city_list.py
@@ -0,0 +1,9 @@
+cities = ["Bucharest", "Chernivtsi", "Tbilisi", "Da Nang", "Tirana"]
+
+for city in cities:
+    print(f"I am considering {city}.")
+
+print("Numbers from range:")
+
+for number in range(5):
+    print(number)
