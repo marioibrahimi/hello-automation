e8d2661dfb36ab7c7fa2ffa5d9b84dd3ce28c863
Mario Ibrahimi <295660683+marioibrahimi@users.noreply.github.com>
2026-07-08 21:49:54 +0300

    Day 20: build city monthly cost calculator

---
+++ city_monthly_calculator.py
@@ -0,0 +1,45 @@
+cities = {
+    "Chernivtsi": {
+        "rent": 300,
+        "food": 250,
+        "transport": 30,
+        "internet": 15,
+
+    },
+    "Bucharest": {
+        "rent": 500,
+        "food": 300,
+        "transport": 50,
+        "internet": 10,
+    },
+    "Tbilisi": {
+        "rent": 450,
+        "food": 280,
+        "transport": 35,
+        "internet": 15,
+
+    },
+}
+
+selected_city = input("Which city do you want to check? ").strip().title()
+
+if selected_city in cities:
+    city_costs = cities[selected_city]
+
+    total = (
+        city_costs["rent"]
+        + city_costs["food"]
+        + city_costs["transport"]
+        + city_costs["internet"]
+    )
+
+    print("City cost estimate")
+    print(f"City: {selected_city}")
+    print(f"Rent: ${city_costs['rent']}")
+    print(f"Food: ${city_costs['food']}")
+    print(f"Transport: ${city_costs['transport']}")
+    print(f"Internet: ${city_costs['internet']}")
+    print(f"Total: ${total}")
+else:
+    print("City not found.")
+    print("Available cities: Chernivtsi, Bucharest, Tbilisi")
